71c8961da41fcfa339bb1b8860e5437f8d5eb649
Ahmad N. F <fahmadnaufal@gmail.com>
2015-09-22 11:19:14 +0700

    Edit lagi ya mif lafyu

--- transmitter.c
+++ transmitter.c
@@ -6,9 +6,16 @@
 
  /* GLOBAL VARIABLES ARE INSIDE THE HEADER */
 
+
+void errorr(char *message) {
+	perror(message);
+	exit(0);
+}
+
 int main(int argc, char *argv[]) {
 	int sockfd, port;		// sock file descriptor and port number
 	pid_t pid;			// process id for forking
+	struct hostent *server;
 
 	if (argc < 4) {
 		// case if arguments are less than specified
@@ -16,11 +23,13 @@ int main(int argc, char *argv[]) {
 		return 1;
 	} else {
 		port = atoi(argv[2]);
-		receiverIP = argv[1];
+		if ((server = gethostbyname(argv[1])) == NULL)
+			errorr("SERVER NULL\n");
 
 		// creating IPv4 data stream socket
-		if (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) < 0) {
-			printf("ERROR: Cannot create socket.\n");
+		printf("[TRANSMITTER] Creating socket to %s Port %s...\n", argv[1], argv[2]);
+		if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+			printf("ERROR: Create socket failed.\n");
 			return 1;
 		}
 
@@ -30,7 +39,7 @@ int main(int argc, char *argv[]) {
 		// initializing the socket host information
 		memset(&receiverAddr, 0, sizeof(receiverAddr));
 		receiverAddr.sin_family = AF_INET;
-		receiverAddr.sin_addr.s_addr = inet_addr(receiverIP);
+		bcopy((char *)server->h_addr, (char *)&receiverAddr.sin_addr.s_addr, server->h_length);
 		receiverAddr.sin_port = htons(port);
 
 		// open the text file
@@ -40,6 +49,9 @@ int main(int argc, char *argv[]) {
 			return 1;
 		}
 
+		if (connect(sockfd,(struct sockaddr *)&receiverAddr,sizeof(receiverAddr)) < 0) 
+        	errorr("CONNECT FAILED :(");
+
 		if ((pid = fork()) > 0) {
 			// this is the parent process
 			// use as char transmitter from the text file
@@ -47,8 +59,9 @@ int main(int argc, char *argv[]) {
 			int counter = 1;
 			while ((buf[0] = fgetc(tFile)) != EOF) {
 				if (isXON) {
-					if (sendto(sockfd, buf, BUFMAX, 0, (const struct sockaddr *) &receiverAddr, receiverAddrLen) != BUFMAX) {
-						printf("ERROR: sendto() sent buffer with size more than expected.\n");
+					int senttt;
+					if ((senttt = sendto(sockfd, buf, BUFMAX, 0, (const struct sockaddr *) &receiverAddr, receiverAddrLen)) != BUFMAX) {
+						printf("ERROR: sendto() sent buffer with size more than expected %d.\n", senttt);
 						return 1;
 					}
 
@@ -57,17 +70,25 @@ int main(int argc, char *argv[]) {
 					printf("Waiting for XON...\n");
 				}
 
-				usleep(1000);	// delaying sendto per loop so behaviors can be seen
+				sleep(1);
+
+				// usleep(1000);	// delaying sendto per loop so behaviors can be seen
 			}
 
+			// sending endfile to receiver, marking the end of data transfer
+			buf[0] = Endfile;
+			sendto(sockfd, buf, BUFMAX, 0, (const struct sockaddr *) &receiverAddr, receiverAddrLen);
+			
 			fclose(tFile);
 			printf("Byte sending done! Closing sockets...\n");
 			close(sockfd);
+			isSocketOpen = 0;
 			printf("Socket Closed!\n");
 
 		} else if (pid == 0) {
+			printf("CHILD RECEIVING HAHA\n");
 			// child process
-			// read if there is XON/XOFF sent by receiver by using sendto
+			// read if there is XON/XOFF sent by receiver using recvfrom()
 			struct sockaddr_in srcAddr;
 			int srcLen = sizeof(srcAddr);
 			while (isSocketOpen) {
